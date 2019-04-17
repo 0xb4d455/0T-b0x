3cb1484788f018262c4d1ea4de9cd672a1ce674e
0xb4d455 <49698243+0xb4d455@users.noreply.github.com>
2019-04-17 22:48:43 +0200

    Update 0T-b0x.js
    Multiple OT-Boxes now supported; Added reset function; Click function optimized; Minor other optimizations;

--- 0T-b0x.js
+++ 0T-b0x.js
@@ -3,7 +3,7 @@
 // @author       0xb4d455
 // @include      https://*pr0gramm.com*
 // @namespace    ot
-// @version      0.1
+// @version      0.2
 // @description  Get notified when there is an OT in the comments
 // @match        http://*/*
 // @grant        none
@@ -11,12 +11,13 @@
 
 var id = "ot-box";
 var otWarning = null;
-var commentBox = null;
+var commentBoxes = [];
+var commentBoxesIndices = 0;
 var searchString = ["OT", "ot", "Offtopic", "OFFTOPIC"];
 var foundString = "OT in den Kommentaren!";
 
 //some good styling here
-var cssAsString = "#ot-box{position:fixed;top:20px;left:40px;padding:10px;max-width:350px;visibility:hidden;font-size:25px;color:#ee4d2e;background-color:#cded2d;z-index:9999;height:60px;vertical-align:baseline;display:flex;align-items:center;justify-content:center;}";
+var cssAsString = "#ot-box{position:fixed;top:20px;left:40px;padding:10px;visibility:hidden;font-size:25px;color:#ee4d2e;background-color:#cded2d;z-index:9999;height:60px;vertical-align:baseline;display:flex;align-items:center;justify-content:center;}";
 
 (function() {
     'use strict';
@@ -31,27 +32,42 @@ var cssAsString = "#ot-box{position:fixed;top:20px;left:40px;padding:10px;max-wi
         //add keypress and click listener
         document.addEventListener('keyup', keyPressFunc, true);
         document.addEventListener('click', clickFunc, true);
-
-        //add scrolling to otbox div
-        otWarning.on('click', scroller);
     }
+
+    //initial search
+    searchComments();
+
+
 })();
 
-function clickFunc(){
-    setTimeout(function(){
-        //check if we should hide the otBox
-        //TODO: temporary fix. needs some overhauling soon...
-        if($(".item-container").length == 0){
-            hideOTWarning();
-        }
-    }, 1000);
+function clickFunc(e){
+    //check if we clicked on otWarning div
+    if(e.target == otWarning[0]){
+        scroller();
+    }else{
+        searchComments();
+    }
 }
 
 function scroller(){
-    commentBox.css("border","1px solid #ee4d2e");
-    commentBox.css("padding","5px");
-    commentBox.get(0).scrollIntoView({block: "center"});
-    hideOTWarning();
+    commentBoxes.forEach(function(element) {
+        element.css("border","1px solid #ee4d2e");
+        element.css("padding","5px");
+    });
+
+    if(commentBoxes.length == 1){
+        commentBoxes[0].get(0).scrollIntoView({block: "center"});
+        return;
+    }
+
+    setTextTo(foundString + "\n ("+(commentBoxesIndices+1)+"/"+commentBoxes.length+")");
+
+    commentBoxes[commentBoxesIndices].get(0).scrollIntoView({block: "center"});
+    commentBoxesIndices++;
+
+    if(commentBoxesIndices >= commentBoxes.length){
+        commentBoxesIndices = 0;
+    }
 }
 
 function hideOTWarning(){
@@ -62,34 +78,69 @@ function showOTWarning(){
     otWarning.css("visibility", "visible");
 }
 
-function searchComments(){
-    var commentDiv = $(".comment-content");
+function searchComments(){    
+    setTimeout(function(){
+        reset();
+        if($(".item-container").length != 0){
+            var commentDiv = $(".comment-content");
 
-    if(commentDiv.length > 0){
-        commentDiv.each(function(){
-            var that = $(this);
-            var commentText = that.text().trim();
+            if(commentDiv.length > 0){
+                commentDiv.each(function(){
+                    var that = $(this);
+                    var commentText = that.text().trim();
 
-            searchString.forEach(function(element) {
-                if(commentText.startsWith(element) == true){
-                    commentBox = that;
-                    showOTWarning();
-                    return;
+                    searchString.forEach(function(element) {
+                        if(commentText.startsWith(element) == true){
+                            commentBoxes.push(that);
+                            showOTWarning();
+                            return;
+                        }
+                    });
+                });
+
+                if(commentBoxes.length == 0){
+                    hideOTWarning();
                 }
-            });
-        });
-    }
+            }
+        } else {
+            hideOTWarning();
+        }
+    }, 500);
 }
 
 function keyPressFunc(evt) {
     //listen for 'a', 'd', left arrow and right arrow
     if ( evt.which == 39 || evt.which == 37 || evt.which == 65 || evt.which == 68) {
+
         //hide on each press
         hideOTWarning();
 
-        //search for OT in comments after timeout
-        setTimeout(searchComments, 1000);
+        //search for OT in comments
+        searchComments();
     }
+
+    //listen for 'o','O' or '0' key to jump to the OT
+    if ( evt.which == 48 || evt.which == 79 || evt.which == 96 || evt.which == 111) {
+        scroller();
+    }
+}
+
+function resetCommentBoxesArrayAndIndices(){
+    commentBoxes = [];
+    commentBoxesIndices = 0;
+}
+
+function setTextTo(newString){
+    otWarning.text(newString);
+}
+
+function resetText(){
+    setTextTo(foundString);
+}
+
+function reset(){
+    resetCommentBoxesArrayAndIndices();
+    resetText();
 }
 
 function loadStyling(){
